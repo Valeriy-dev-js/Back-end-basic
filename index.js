b8c1b880bbaf86a12b7d5a4ad6bf180e0c936913
Valeriy-dev-js <vorobvdev@gmail.com>
2021-05-27 13:34:24 +0300

    Starting with API

--- index.js
+++ index.js
@@ -1,35 +1,22 @@
 import express from 'express';
-import fs from 'fs'
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('Middleware 1');
-    next()
-})
 
-// app.use('/about', (req, res, next) => {
-//     console.log('Middleware About');
-//     res.send("About")
-    
-// })
-
-app.use((req, res , next) => {
-    const now = Date.now()
-    const data = `Data now: ${now}`
-    console.log(data);
-    fs.appendFile("server.log", data + '\n', ()=>{})
-    next
-})
-
-
-
-
-
-
+// app.use('/about', (req, res, next) => {//     console.log('Middleware About');//     res.send("About")    // })
 
 app.get('/', (req, res) =>{
     // console.log('Route/ ');
-    res.send('hello')
+    res.send('<h1>Hello!</h1>')
+    
 })
+
+app.use('/about', (req, res) => {
+    const id = req.query.id;
+    res.send('<h1>Hello!</h1><p>id = ' + id + '</p>')
+})
+
+
+
+
 app.listen(3000)
---
+++ routes/Tasck.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+
+const Tasck = Router()
+
+Tasck.get('/v1/tasks', (req, res) =>{
+    
+})
