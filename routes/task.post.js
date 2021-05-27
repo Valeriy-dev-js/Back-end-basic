34b4c76e9f6e3e716f0939348d589270acc1e83c
Few <valera.vorobev.20@gmail.com>
2021-05-28 00:37:56 +0300

    Add GET and POST json

--- routes/task.post.js
+++ routes/task.post.js
@@ -1,8 +1,25 @@
 import { Router } from "express";
-import { postTask } from '../controllers/postTask.js'
+import fs from 'fs';
 
 const router = Router();
 
-router.post('/task', postTask);
+router.post('/task', (req, res) =>{
+    const body = req.body
+    fs.readFile('Tasks.json', 'utf-8', (err, data) => {
+        if(err) {
+            throw err;
+        };
+
+        const tacks = JSON.parse(data)
+        const newTasks = JSON.stringify([...tacks, body])
+
+        fs.writeFile(`Tasks.json`, newTasks, err => {
+            if(err) {
+                throw err;
+            };
+        });
+    });
+    res.send(`Name: ${body.name} Done: ${body.done}`);
+});
 
 export default router;
--- routes/tasks.get.js
+++ routes/tasks.get.js
@@ -1,7 +1,17 @@
 import { Router } from "express";
-import { getTasks } from '../controllers/getTask.js'
+import fs from 'fs';
 const router = Router();
 
-router.get('/tasks', getTasks);
+router.get('/tasks', (req, res) => {
+    fs.readFile('Tasks.json', 'utf-8', (err, data) => {
+        if(err) {
+            throw err;
+        };
+        const todos = JSON.parse(data)
+        const test = data
+        res.send(todos)
+    });
+});
+
 
 export default router;
