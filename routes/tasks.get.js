de63798c4726693579863740f856ffd5e3b0cb72
Valeriy-dev-js <vorobvdev@gmail.com>
2021-05-28 16:22:32 +0300

    Change sortingFiltring logic

--- routes/tasks.get.js
+++ routes/tasks.get.js
@@ -4,40 +4,30 @@ const router = Router();
 
 
 router.get('/tasks', (req, res) => {
-    const { filterBy, order } = req.query
+    const { filterBy, order = 'desc' } = req.query
+
     fs.readFile('Tasks.json', 'utf-8', (err, data) => {
         if (err) {
             throw err;
         };
         const tasks = JSON.parse(data);
 
-        let filteredTasks;
+        let filteredTasks = tasks;
 
-        filterBy
-        ? filteredTasks = tasks.filter(task => {
-            if(filterBy === 'done'){
-                return task.done === true;
-            };
-            if(filterBy === 'undone'){
-                return task.done === false;
-            };
-        })
-        : filteredTasks = tasks;
+        if (filterBy) {
+            filteredTasks = tasks.filter(task =>
+            filterBy === 'done'
+                ? task.done === true
+                : task.done === false)
+        }
 
-        let sorteredTasks;
-
-        order 
-        ? sorteredTasks = filteredTasks.sort((a, b) =>{
-            if(order === 'desc'){
-                return Date.parse(a.date) - Date.parse(b.date);
-            };
-            if(order === 'asc'){
-                return Date.parse(b.date) - Date.parse(a.date);
-            };
-        })
-        : sorteredTasks = filteredTasks;
-
-        res.send(sorteredTasks)
+        if (order) {
+            filteredTasks = filteredTasks.sort((a, b) =>
+            order === 'desc'
+                ? Date.parse(a.date) - Date.parse(b.date)
+                : Date.parse(b.date) - Date.parse(a.date))
+        }
+        res.send(filteredTasks)
     });
 });
 
