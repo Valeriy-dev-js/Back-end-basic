63eada65fd977fa634f8d8c4ff91ef82e1e038a1
Valeriy-dev-js <vorobvdev@gmail.com>
2021-05-28 17:37:08 +0300

    Add validator to POST

--- routes/task.post.js
+++ routes/task.post.js
@@ -1,26 +1,34 @@
 import { Router } from "express";
+import { body, validationResult} from 'express-validator';
 import fs from 'fs';
 import { v4 as uuidv4 } from 'uuid'
 const router = Router();
 
-router.post('/task', (req, res) => {
-    fs.readFile('Tasks.json', 'utf-8', (err, data) => {
-        if (err) {
-            throw err;
-        };
-        
-        const task = req.body
-        const tasks = JSON.parse(data)
-        const newTasks = [...tasks, { id: uuidv4(), ...task, date: new Date(Date.now()) }];
-        const prettyJSON = JSON.stringify(newTasks, null, 2)
-
-        fs.writeFile(`Tasks.json`, prettyJSON, err => {
+router.post('/task',
+    body('name').isString().isLength({ min: 2}),
+    body('done').isBoolean(),
+    (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).send({errors: errors.array()});
+    };
+        fs.readFile('Tasks.json', 'utf-8', (err, data) => {
             if (err) {
                 throw err;
             };
+            
+            const task = req.body
+            const tasks = JSON.parse(data)
+            const newTasks = [...tasks, { id: uuidv4(), ...task, date: new Date(Date.now()) }];
+            const prettyJSON = JSON.stringify(newTasks, null, 2)
+
+            fs.writeFile(`Tasks.json`, prettyJSON, err => {
+                if (err) {
+                    throw err;
+                };
+            });
+            // res.send(`Name: ${task.name} Done: ${task.done} Length: ${newTasks.length}`);
         });
-        res.send(`Name: ${task.name} Done: ${task.done} Length: ${newTasks.length}`);
-    });
 });
 
 export default router;
