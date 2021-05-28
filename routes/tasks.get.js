9cfe822424b510b60559f17624e112debeb7c5a1
Valeriy-dev-js <vorobvdev@gmail.com>
2021-05-28 15:35:53 +0300

    Change sortingFiltring logic

--- routes/tasks.get.js
+++ routes/tasks.get.js
@@ -5,36 +5,38 @@ const router = Router();
 
 router.get('/tasks', (req, res) => {
     const { filterBy, order } = req.query
-
-
-
-
-
-
     fs.readFile('Tasks.json', 'utf-8', (err, data) => {
         if (err) {
             throw err;
         };
-        const tasks = JSON.parse(data)
-        const filteredTasks = tasks.filter(task => {
+        const tasks = JSON.parse(data);
+
+        let filteredTasks;
+
+        filterBy
+        ? filteredTasks = tasks.filter(task => {
             if(filterBy === 'done'){
                 return task.done === true;
             };
             if(filterBy === 'undone'){
                 return task.done === false;
             };
-            return task;
-        });
+        })
+        : filteredTasks = tasks;
 
-        const sorteredTasks = filteredTasks.sort((a, b) =>{
+        let sorteredTasks;
+
+        order 
+        ? sorteredTasks = filteredTasks.sort((a, b) =>{
             if(order === 'desc'){
                 return Date.parse(a.date) - Date.parse(b.date);
             };
             if(order === 'asc'){
                 return Date.parse(b.date) - Date.parse(a.date);
             };
-            return [...filteredTasks];
-        });
+        })
+        : sorteredTasks = filteredTasks;
+
         res.send(sorteredTasks)
     });
 });
