d9f676bcbcd992a6a6bce9c5dbb4b420556f1336
Valeriy-dev-js <vorobvdev@gmail.com>
2021-05-28 17:54:43 +0300

    Add validator to GET

--- routes/tasks.get.js
+++ routes/tasks.get.js
@@ -1,35 +1,39 @@
 import { Router } from "express";
+import { query, validationResult } from 'express-validator';
 import fs from 'fs';
 const router = Router();
 
 
-router.get('/tasks', (req, res) => {
-    const { filterBy, order = 'desc' } = req.query
+router.get('/tasks',
+    query('filterBy').isString().optional({ checkFalsy: true }),
+    query('order').isString().optional({ checkFalsy: true }),
+    (req, res) => {
+        const { filterBy, order = 'desc' } = req.query
 
-    fs.readFile('Tasks.json', 'utf-8', (err, data) => {
-        if (err) {
-            throw err;
-        };
-        const tasks = JSON.parse(data);
+        fs.readFile('Tasks.json', 'utf-8', (err, data) => {
+            if (err) {
+                throw err;
+            };
+            const tasks = JSON.parse(data);
 
-        let filteredTasks = tasks;
+            let filteredTasks = tasks;
 
-        if (filterBy) {
-            filteredTasks = tasks.filter(task =>
-            filterBy === 'done'
-                ? task.done === true
-                : task.done === false)
-        }
+            if (filterBy) {
+                filteredTasks = tasks.filter(task =>
+                    filterBy === 'done'
+                        ? task.done === true
+                        : task.done === false)
+            }
 
-        if (order) {
-            filteredTasks = filteredTasks.sort((a, b) =>
-            order === 'desc'
-                ? Date.parse(a.date) - Date.parse(b.date)
-                : Date.parse(b.date) - Date.parse(a.date))
-        }
-        res.send(filteredTasks)
+            if (order) {
+                filteredTasks = filteredTasks.sort((a, b) =>
+                    order === 'desc'
+                        ? Date.parse(a.date) - Date.parse(b.date)
+                        : Date.parse(b.date) - Date.parse(a.date))
+            }
+            res.send(filteredTasks)
+        });
     });
-});
 
 
 export default router;
