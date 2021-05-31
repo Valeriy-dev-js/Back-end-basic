8f672bab3eff03c0757a993fd2433aabc6d46216
Valeriy-dev-js <vorobvdev@gmail.com>
2021-05-31 12:05:54 +0300

    Prepare for Heroku deploy

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import patchTask from './routes/task.patch.js'
 
 
 const app = express();
+const PORT = process.env.PORT || 80
 
 app.use(express.json())
 app.use(getTasks);
@@ -17,4 +18,4 @@ app.use(patchTask)
 
 
 
-app.listen(3000, () => console.log('Server listening on port 3000...'))
+app.listen(PORT, () => console.log('Server listening on port 3000...'))
