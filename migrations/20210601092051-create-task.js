4cbc3d01b41d061f169cb26e3fac2f9207590cef
Valeriy-dev-js <vorobvdev@gmail.com>
2021-06-01 13:00:55 +0300

    Add model

---
+++ migrations/20210601092051-create-task.js
@@ -0,0 +1,32 @@
+'use strict';
+const DataTypes = require('sequelize');
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Tasks', {
+      id: {
+        allowNull: false,
+        type: DataTypes.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true
+      },
+      name: {
+        type: Sequelize.STRING,
+        unique: true
+      },
+      done: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Tasks');
+  }
+};
