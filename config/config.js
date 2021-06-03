4a644ce84ba61650a8846ad4b2dd08458bbf997d
Valeriy-dev-js <vorobvdev@gmail.com>
2021-06-03 13:49:22 +0300

    Change Config

--- config/config.js
+++ config/config.js
@@ -19,6 +19,10 @@ module.exports =  {
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
     host: process.env.DATABASE_HOST,
-    dialect: "postgres"
+    dialect: "postgres",
+    ssl: true,
+        dialectOptions: {
+            ssl: true
+        }
   }
 }
