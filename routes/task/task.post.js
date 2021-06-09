d2e9e436a7671fdcac0e9fc2f9decb3f957b0f58
Valeriy-dev-js <vorobvdev@gmail.com>
2021-06-09 19:35:41 +0300

    Refactoring Post

--- routes/task/task.post.js
+++ routes/task/task.post.js
@@ -13,18 +13,18 @@ router.post('/task',
     body('done').isBoolean(),
     async (req, res, next) => {
         try {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            throw new ErrorHandler(400, "invalid body", errors.array());
-        };
-        const userUUID = res.locals.user.uuid;
-        const {name, done} = req.body;
-        const taskName = await Task.findOne({where: {name: name, user_uuid:userUUID}});
-        if(taskName){
-            throw new ErrorHandler(422, "Task with same name not allowed");
-        }
-        const task = await Task.create({ user_uuid: userUUID , name, done});
-        return res.json(task);
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                throw new ErrorHandler(400, "invalid body", errors.array());
+            };
+            const user_uuid = res.locals.user.uuid;
+            const { name, done } = req.body;
+            const exsistingTask = await Task.findOne({ where: { name, user_uuid } });
+            if (exsistingTask) {
+                throw new ErrorHandler(422, "Task with same name not allowed");
+            }
+            const task = await Task.create({ user_uuid, name, done });
+            return res.json(task);
         } catch (err) {
             next(err)
         };
