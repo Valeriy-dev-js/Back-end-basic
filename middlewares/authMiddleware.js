0a3047fea1e3db53b2364bb3edea7b8fd477ffbc
Valeriy-dev-js <vorobvdev@gmail.com>
2021-06-09 19:21:51 +0300

    Refactoring authMiddleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,28 +1,29 @@
 require('dotenv').config()
+const { User } = require('../models')
 const { ErrorHandler } = require('../error');
 const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
-    if (req.method === 'OPTIONS') {
-        next();
-    };
-
     try {
-        if(!req.headers.authorization){
+        if (!req.headers.authorization) {
             throw new ErrorHandler(403, "Missing headers");
         }
+
         const token = req.headers.authorization.split(' ')[1]
-            if (!token) {
+        if (!token) {
             throw new ErrorHandler(403, "User isn`t authorized");
-            };
-        jwt.verify(token, process.env.SECRET, (err, decoded) => {
-            if (err) {
-                throw new ErrorHandler(422, "Incorrect token");
-            };
-            res.locals.user = decoded;
-        });
+        };
+        try {
+            const decoded = jwt.verify(token, process.env.SECRET)
+            const existingUser = User.findOne({ where: { uuid: decoded.uuid } })
+            if (!existingUser) {
+                throw new ErrorHandler(403, "User isn`t authorized")
+            }
+            res.locals.user = decoded
+        } catch (err) {
+            throw new ErrorHandler(422, "Incorrect token")
+        }
         next()
     } catch (err) {
-
         next(err);
     };
 };
