ccef9ff2179b1bc46167a154d4177935a442cb3f
Valeriy-dev-js <vorobvdev@gmail.com>
2021-06-11 11:33:22 +0300

    Add validationMiddleware to DELETE

--- routes/task/task.delete.js
+++ routes/task/task.delete.js
@@ -3,32 +3,24 @@ const { Task } = require('../../models');
 const { param, validationResult } = require('express-validator');
 const { ErrorHandler } = require('../../error')
 const authMiddleware = require('../../middlewares/authMiddleware');
-
-
+const validatorMiddleware = require('../../middlewares/validatorMiddleware');
 const router = Router();
-
-
 
 router.delete('/task/:uuid',
     authMiddleware,
-    param('uuid').isUUID(),
+    param('uuid').isUUID().withMessage('Invalid uuid'),
+    validatorMiddleware,
     async (req, res, next) => {
+        const user_uuid = res.locals.user.uuid;
+        const uuid = req.params.uuid;
         try {
-            const errors = validationResult(req);
-            if (!errors.isEmpty()) {
-                throw new ErrorHandler(400, "invalid request", errors.array())
-            };
-
-            const user_uuid = res.locals.user.uuid;
-            const uuid = req.params.uuid;
-
             const exsistingTask = await Task.findOne({ where: { uuid, user_uuid } });
             if (!exsistingTask) {
                 throw new ErrorHandler(422, "Can`t find task");
             };
-
+            const task = exsistingTask.dataValues
             await Task.destroy({ where: { uuid, user_uuid } });
-            return res.send("Task deleted")
+            return res.send(task)
         } catch (err) {
             next(err);
         };
