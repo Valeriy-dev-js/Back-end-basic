bb383aed312d61ff86add007ea5f59cfe6a1e09a
Valeriy-dev-js <vorobvdev@gmail.com>
2021-06-11 11:50:44 +0300

    Add validationMiddleware to GET

--- routes/task/tasks.get.js
+++ routes/task/tasks.get.js
@@ -1,47 +1,39 @@
 const { Router } = require("express");
 const { Task } = require('../../models')
 const { User } = require('../../models')
-const { query, validationResult } = require('express-validator');
+const { query } = require('express-validator');
 const { ErrorHandler } = require('../../error');
-const authMiddleware = require('../../middlewares/authMiddleware')
-
-
+const authMiddleware = require('../../middlewares/authMiddleware');
+const validatorMiddleware = require('../../middlewares/validatorMiddleware');
 const router = Router();
 
 router.get('/tasks',
     authMiddleware,
-    query('order').isString().isIn(['asc', 'desc']).toUpperCase().optional({ checkFalsy: true }),
-    query('filterBy').isString().isIn(['done', 'undone']).optional({ checkFalsy: true }),
-    query('curentPage').isNumeric().optional({ checkFalsy: true }),
-    query('limit').isNumeric().optional({ checkFalsy: true }),
+    query('order').isString().isIn(['asc', 'desc']).toUpperCase().optional({ checkFalsy: true }).withMessage('Invalid order'),
+    query('filterBy').isString().isIn(['done', 'undone']).optional({ checkFalsy: true }).withMessage('Invalid filterBy'),
+    query('curentPage').isNumeric().optional({ checkFalsy: true }).withMessage('Invalid curentPage'),
+    query('limit').isNumeric().optional({ checkFalsy: true }).withMessage('Invalid limit'),
+    validatorMiddleware,
     async (req, res, next) => {
+        const user_uuid = res.locals.user.uuid;
+        const { filterBy, order, curentPage = 1, limit = 100 } = req.query;
+        const filterQuery = { 'done': true, 'undone': false };
+        const filter = {
+            where: { user_uuid },
+            offset: limit * (curentPage - 1),
+            limit: limit
+        };
+        if (order) filter.order = [['createdAt', order]];
+        if (filterBy) filter.where.done = filterQuery[filterBy];
         try {
-            const errors = validationResult(req);
-            if (!errors.isEmpty()) {
-                throw new ErrorHandler(400, "qerry", errors.array())
-            };
-            const user_uuid = res.locals.user.uuid;
             const { name } = await User.findOne({ where: { uuid: user_uuid } });
-            const { filterBy, order, curentPage = 1, limit = 100 } = req.query;
-            const filterQuery = { 'done': true, 'undone': false };
-
-            const filter = {
-                where: { user_uuid },
-                offset: limit * (curentPage - 1),
-                limit: limit
-            };
-            if (order) filter.order = [['createdAt', order]];
-            if (filterBy) filter.where.done = filterQuery[filterBy];
-
             const { count, rows } = await Task.findAndCountAll(filter);
-
             const pagesCount = Math.ceil(count / limit);
-            return res.send({ pagesCount, userName: name, tasks: rows })
+            return res.send({ pagesCount, tasks: rows })
 
         } catch (err) {
             next(err);
         };
     });
-
 
 module.exports = router;
