bc8481970e1c2ff729c68a72ab530fe7edf0923c
Valeriy-dev-js <vorobvdev@gmail.com>
2021-06-11 15:33:58 +0300

    Add setter to user model

--- models/user.js
+++ models/user.js
@@ -1,16 +1,11 @@
 'use strict';
+const bcrypt = require('bcrypt')
 const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
     }
   };
   User.init({
@@ -26,7 +21,10 @@ module.exports = (sequelize, DataTypes) => {
     },
     password: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      set(value) {
+        this.setDataValue('password', bcrypt.hashSync(value, 5));
+      }
     }
   }, {
     sequelize,
